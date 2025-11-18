8684210712898a624cb5d2dd2970a21cf6647e78
dacheangel <dacheangel18@gmail.com>
2025-11-18 12:24:47 +0300

    Add files via upload

---
+++ FitnessApp.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class FitnessAp{
+    public static String getFitnessLevel(int steps){
+        if(steps>=10000){
+            return "Excellent";
+        }
+        else if(steps>=7000){
+            return"Good";
+        }
+        else if(steps>=4000){
+            return"Average";
+            }
+        else{
+            return"Poor";
+        }
+    }
+    public static void main(String[]args){
+        Scanner scanner=new Scanner(System.in);{
+        System.out.print("Enter your steps for today:");
+        int steps=scanner.nextInt();
+       String level=getFitnessLevel(steps);
+        System.out.println("Your fitness level"+level);
+        scanner.close();
+    }
+}
+}
